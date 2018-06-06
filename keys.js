19554eedd2399f257b2aea86ddaceaaad8f5baea
Stanley Louis <stanlouis@msn.com>
2018-06-05 20:17:38 -0400

    Initialize repository

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitter = {
+  consumerkey: process.env.TWITTERCONSUMERKEY,
+  consumersecret: process.env.TWITTERCONSUMERSECRET,
+  accesstokenkey: process.env.TWITTERACCESSTOKENKEY,
+  accesstokensecret: process.env.TWITTERACCESSTOKENSECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFYID,
+  secret: process.env.SPOTIFYSECRET
+};
