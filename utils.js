e0818f2130c1c2041aaa8ee13b9cffdf125c2338
Stanley Louis <stanlouis@msn.com>
2018-06-06 19:24:11 -0400

    format showMovieInfo

--- utils.js
+++ utils.js
@@ -35,7 +35,6 @@ module.exports = {
 
   showMovieInfo: (movie) => {
     movie = urlencode(movie);
-    console.log(movie)
     let url = 'http://www.omdbapi.com/?t='+movie+'&y=&plot=short&apikey=trilogy'
     request(
       url,
@@ -44,7 +43,18 @@ module.exports = {
         if (!error && response.statusCode === 200) {
           // Parse the body of the site and recover just the imdbRating
           // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+          const bodyObj = JSON.parse(body);
+          console.log(`
+          Movie's title: ${bodyObj.Title}
+          Movie's title: ${bodyObj.Year}
+          Movie's IMDB rating: ${bodyObj.imdbRating}
+          Movie's Rotten Tomatoes rating: ${bodyObj.Ratings[1]['Value']}
+          Movie's country: ${bodyObj.Country}
+          Movie's language: ${bodyObj.Language}
+          Movie's plot: ${bodyObj.Plot}
+          Movie's actors: ${bodyObj.Actors}
+          
+          `);
         }
       }
     );
